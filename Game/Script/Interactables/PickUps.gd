7f32bd38aa34c9bc2ae92632d138e27a01359797
Embermane <107211970+Embermane42@users.noreply.github.com>
2022-10-16 16:33:47 +0200

    Add files via upload

---
+++ Game/Script/Interactables/PickUps.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+var inventory = preload("res://Game/Script/Inventory/Inventory.gd")
+var inventoryDisplay = preload("res://Game/Script/Inventory/InventoryDisplay.gd")
+
+export(Resource) var item = null
+
+func _on_PickUps_body_entered(body):
+	if body.name == 'Player':
+		add_to_inventory(item)
+		queue_free()
+
+func add_to_inventory(pickup):
+	for i in inventory.items:
+		if inventory.items[i] == null:
+			inventory.set_item(i, pickup)
+			inventoryDisplay.update_inventory_display()
